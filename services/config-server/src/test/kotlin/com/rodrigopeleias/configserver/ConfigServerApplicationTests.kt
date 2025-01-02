e8831fe52d97699a85f665a66658fa5983811b27
Rodrigo Peleias <rodrigopeleias@MacBook-Air-de-Rodrigo-2.local>
2025-01-02 16:37:02 -0300

    feat: add initial .gitignore file for Spring Boot project

---
+++ services/config-server/src/test/kotlin/com/rodrigopeleias/configserver/ConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
